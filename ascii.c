40be2a726b1701a73071960b357941bf704f7244
Manas Khanna <manas15khanna@gmail.com>
2024-01-03 09:25:03 +0530

    Deleted old files to make new files

--- ascii.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    char x;
-    printf("Enter the character:");
-    scanf("%c", &x);
-    printf("The ASCII value of the character is %d\n", x);
-    return 0;
-}
