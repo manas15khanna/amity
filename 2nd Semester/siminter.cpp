5335a664ec3b0125fa99df14c8a8863b52515f30
manas15khanna <manas15khanna@gmail.com>
2024-02-22 19:34:42 +0530

    interest modified

--- 2nd Semester/siminter.cpp
+++ 2nd Semester/siminter.cpp
@@ -1,19 +1,19 @@
 //21 Feb
 #include <cmath>
 #include <iostream>
+using namespace std;
 int main()
 {
   float p;
   float t, r;
-  std::cout << "Enter the principle amount, time and rate of interest:" << std::endl;
-  std::cin >> p;
-  std::cin >> t;
-  std::cin >> r;
-  float total=p*r*t;
-  float simp=total/100;
-  float comp= p* pow(1+r/100, t);
-  std::cout << "The total amount is " << total << std::endl;
-  std::cout << "The Simple interest is " << simp << std::endl;
-  std::cout << "Compound interest is "<< comp << std::endl;
+  cout << "Enter the principle amount, time and rate of interest:" << endl;
+  cin >> p;
+  cin >> t;
+  cin >> r;
+  float simple=(p*r*t)/100;
+  float comp= p*((pow((1 + r / 100), t)));
+  float ci=comp-p;
+  cout << "The Simple interest is " << simple << endl;
+  cout << "Compound interest is "<< ci << endl;
   return 0;
 }
