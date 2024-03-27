f2f1b9810d8d3f8211b432f66b0697b075371da0
Manas <manas15khanna@gmail.com>
2024-03-27 10:21:56 +0530

    friendclass2

--- 2nd Semester/friendclass2.cpp
+++ 2nd Semester/friendclass2.cpp
@@ -1,22 +1,29 @@
 #include <iostream>
 using namespace std;
+class A;
+class B;
 class A{
+  int balance;
 public:
-  int money=10;
-  friend int C(A);
+  A(){
+    balance = 10;
+  }
+  friend int C(A,B);
 };
 class B{
+  int balance;
 public:
-    int money=20;
-    friend int C(A, B);
+  B(){
+    balance = 20;
+  }
+  friend int C(A,B);
 };
-int C(A a, B b)
-{
-  return a.money+b.money;
+int C(A a,B b){
+  return a.balance + b.balance;
 }
-int main()
-{
+int main(){
   A obj1;
-  B obj2;
-  cout <<  C(obj1, obj2) << endl;
+  B obj2; 
+  cout<< C(obj1,obj2)<<endl;
 }
+
